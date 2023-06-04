2d9e309db9129f368c9ffe91d6a16ff7a0ebca98
DeenoDev <deyanoklaasen@outlook.com>
2023-06-04 23:34:54 +0200

    Used DOM manipulation to add style to the keys when pressed.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return; //stop the function from running all together
+  audio.currentTime = 0; // rewind to the start.
   audio.play();
+  key.classList.add('playing');
 });
