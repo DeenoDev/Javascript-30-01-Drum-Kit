40819d3cfae1193e2ea75c1a57d53b39089b41c1
DeenoDev <deyanoklaasen@outlook.com>
2023-06-05 23:53:32 +0200

    Project completed and ready for push to main.

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-window.addEventListener('keydown', function(e){
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) return; //stop the function from running all together
-  audio.currentTime = 0; // rewind to the start.
-  audio.play();
-  key.classList.add('playing');
-});
-
-function removeTransition(e){
-  if (e.propertyName !== 'transform') return; //skip it if its not a transform
-  this.classList.remove('playing');
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitioned', removeTransition));
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
